c52657938e019a729da5b913bd7759106dc64220
GAURAV RAMI <gauravrami@gauravs-macbook-pro.local>
2018-04-16 02:10:07 +0530

    Initial Commit

---
+++ vdoTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vdoTest
+//
+//  Created by GAURAV RAMI on 16/04/18.
+//  Copyright © 2018 GAURAV RAMI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
